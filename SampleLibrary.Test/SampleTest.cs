661a0bc991abf16ddb597faef83fd20406ec6dca
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2014-02-27 01:43:47 +0900

    initial commit

---
+++ SampleLibrary.Test/SampleTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace SampleLibrary.Test
+{
+	[TestFixture]
+	public class SampleTest
+	{
+		[TestCase]
+		public void TestSay()
+		{
+			var sample = new Sample();
+			Assert.AreEqual("Hello TDD Boot Camp!", sample.Say());
+		}
+	}
+}
+
